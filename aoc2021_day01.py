c0e3dfdf0f6eb4f3a8cf78e3b1dad9b8504157ae
edforth <ed@forthplace.com>
2021-12-01 00:10:53 -0500

    Day 1

--- aoc2021_day01.py
+++ aoc2021_day01.py
@@ -1,8 +1,26 @@
-with open('day02input.txt','r') as f:
+with open('day01input.txt','r') as f:
     lines = f.read().splitlines()
 
 #Part 1
+inccount = 0
+for i in range(1,len(lines)):
+    if int(lines[i]) > int(lines[i-1]):
+        inccount=inccount + 1
+print("Part 1 answer = ",inccount)
+
 
 
 
 #print("Part 1 Answer =")
+#Correct first try (1390)
+
+#Part 2
+
+suminccount = 0 
+for i in range(0,len(lines)-3):
+    suma = int(lines[i]) + int(lines[i+1]) + int(lines[i+2])
+    sumb = int(lines[i+1]) + int(lines[i+2]) + int(lines[i+3])
+    if sumb > suma:
+        suminccount = suminccount + 1
+print("Part 1 answer = ",suminccount)
+#Correct first try (1457)
